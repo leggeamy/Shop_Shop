9ae28aafab487f5f68d9ee61e76709e51a79ebb6
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2021-02-14 18:05:37 -0500

    changes to implement stripe

--- Server/schemas/resolvers.js
+++ Server/schemas/resolvers.js
@@ -1,7 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
-
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
@@ -54,26 +53,17 @@ const resolvers = {
       throw new AuthenticationError('Not logged in');
     },
     checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referrer).origin;
       const order = new Order({ products: args.products });
-      const { products } = await.order.populate('products').execPopulate();
-      
       const line_items = [];
 
+      const { products } = await order.populate('products').execPopulate();
+
       for (let i=0; i < products.length; i++) {
-        const session = await stripe.checkout.sessions.create({
-          payment_method_types: ['card'],
-          line_items,
-          mode: 'payment',
-          success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-          cancel_url: 'https://example.com/cancel'
-      });
-
-      return { session: session.id };
-
-        //generate product id
         const product = await stripe.products.create({
           name: products[i].name,
-          description: products[i].description
+          description: products[i].description,
+          images: [`${url}/images/${products[i].image}`]
         });
 
         // generate price id using the product id
@@ -90,6 +80,15 @@ const resolvers = {
         });
       }
 
+      const session = await stripe.checkout.sessions.create({
+          payment_method_types: ['card'],
+          line_items,
+          mode: 'payment',
+          success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${url}/`
+      });
+
+      return { session: session.id };
     }
   },
   Mutation: {
