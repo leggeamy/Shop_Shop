00f18a5be35f0a676a6e53b8bc935cdffa836aff
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2021-02-15 20:16:55 -0500

    implementing redux store

--- Client/src/utils/reducers.js
+++ Client/src/utils/reducers.js
@@ -1,4 +1,3 @@
-import { useReducer } from "react";
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -11,7 +10,7 @@ import {
   TOGGLE_CART
 } from "./actions";
 
-export const reducer = (state, action) => {
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_PRODUCTS:
       return {
